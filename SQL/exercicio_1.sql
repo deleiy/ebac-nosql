5c2a1bc7966f82cca889a0843b55536452b9f019
vanderlei <derlei1.vanderlei@gmail.com>
2026-08-17 20:07:07 -0300

    Atualiza consultas SQL

--- SQL/exercicio_1.sql
+++ SQL/exercicio_1.sql
@@ -1,15 +1,37 @@
--- 1. Quantidade de artistas que possuem músicas com duração maior que 120000 ms
+-- Exercício 1
 
-SELECT COUNT(*) AS Total_Artistas
-FROM (
-    SELECT
-        ar.Name AS Artista,
-        COUNT(t.TrackId) AS Musicas_Mais_120000ms
-    FROM artists ar
-    INNER JOIN albums al
-        ON ar.ArtistId = al.ArtistId
-    INNER JOIN tracks t
-        ON al.AlbumId = t.AlbumId
-    WHERE t.Milliseconds > 120000
-    GROUP BY ar.ArtistId, ar.Name
-);
+-- 1. Lista de filmes e suas categorias correspondentes
+
+SELECT
+    t.Name AS Filme,
+    g.Name AS Categoria
+FROM tracks t
+INNER JOIN genres g
+    ON t.GenreId = g.GenreId;
+
+
+-- 2. Lista de atores com a quantidade de filmes em que participaram
+
+SELECT
+    ar.Name AS Ator,
+    COUNT(al.AlbumId) AS Quantidade_Filmes
+FROM artists ar
+INNER JOIN albums al
+    ON ar.ArtistId = al.ArtistId
+GROUP BY ar.ArtistId, ar.Name
+ORDER BY Quantidade_Filmes DESC;
+
+
+-- 3. Atores que participaram de filmes com duração maior que 120 minutos
+
+SELECT
+    ar.Name AS Ator,
+    COUNT(t.TrackId) AS Quantidade_Filmes
+FROM artists ar
+INNER JOIN albums al
+    ON ar.ArtistId = al.ArtistId
+INNER JOIN tracks t
+    ON al.AlbumId = t.AlbumId
+WHERE t.Milliseconds > 7200000
+GROUP BY ar.ArtistId, ar.Name
+ORDER BY Quantidade_Filmes DESC;
--- SQL/exercicio_2.sql
+++ SQL/exercicio_2.sql
@@ -1,6 +1,8 @@
--- 2. Quantidade de filmes cadastrados
+-- Exercício 2
 
-SELECT COUNT(*) AS Total_Filmes
+-- 1. Quantidade de filmes e categorias
+
+SELECT COUNT(*) AS Total_Registros
 FROM (
     SELECT
         t.Name,
@@ -8,4 +10,35 @@ FROM (
     FROM tracks t
     INNER JOIN genres g
         ON t.GenreId = g.GenreId
+);
+
+
+-- 2. Quantidade de atores encontrados
+
+SELECT COUNT(*) AS Total_Atores
+FROM (
+    SELECT
+        ar.Name,
+        COUNT(al.AlbumId)
+    FROM artists ar
+    INNER JOIN albums al
+        ON ar.ArtistId = al.ArtistId
+    GROUP BY ar.ArtistId, ar.Name
+);
+
+
+-- 3. Quantidade de atores com filmes acima de 120 minutos
+
+SELECT COUNT(*) AS Total_Atores
+FROM (
+    SELECT
+        ar.Name,
+        COUNT(t.TrackId)
+    FROM artists ar
+    INNER JOIN albums al
+        ON ar.ArtistId = al.ArtistId
+    INNER JOIN tracks t
+        ON al.AlbumId = t.AlbumId
+    WHERE t.Milliseconds > 7200000
+    GROUP BY ar.ArtistId, ar.Name
 );
